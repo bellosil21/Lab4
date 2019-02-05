193f24d4181ee721b40593cffa9407fc0f0a5b56
smokeburned <smokeburned@gmail.com>
2019-02-04 20:46:19 -0800

    added overloaded apply method in brightness filter

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
@@ -1,5 +1,6 @@
 package edu.up.cs371.epp.photofunpattern;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 
 /**
@@ -23,12 +24,54 @@ public class BrightnessFilter extends PhotoFilter {
     * @param inPixel is a 32 bit pixel that contains RGB color values
     * @return a new Pixel in which each of the RGB components has been increased
     */
-    public int transformPixel(int inPixel) {
-        int red = constrain(Color.red(inPixel) + ADJUSTMENT);
-        int green = constrain(Color.green(inPixel) + ADJUSTMENT);
-        int blue = constrain(Color.blue(inPixel) + ADJUSTMENT);
-        int outPixel = Color.argb(Color.alpha(inPixel), red, green, blue);
-        return outPixel;
+    public int transformPixel(int inPixel0, int inPixel1, int inPixel2, int inPixel3, int inPixel4,
+                              int inPixel5, int inPixel6, int inPixel7, int inPixel8) {
+
+
+
+
+
+        int outPixel4 = inPixel0 * 1 + inPixel1 * 1 + inPixel2 * -1 + inPixel3 * 1 + inPixel4 * -2 +
+                inPixel5 * -1 + inPixel6 * 1 + inPixel7 * 1 + inPixel8 * -1;
+
+        return outPixel4;
     }
 
+    public Bitmap apply(Bitmap inBmp) {
+        int width = inBmp.getWidth();
+        int height = inBmp.getHeight();
+
+        Bitmap newBmp = Bitmap.createBitmap(width, height, inBmp.getConfig());
+
+        for (int w = 1; w < width-1; w++) {
+            for (int h = 1; h < height-1; h++) {
+                int inPixel0, inPixel1, inPixel2, inPixel3, inPixel4, inPixel5, inPixel6,
+                        inPixel7, inPixel8, outPixel;
+
+                inPixel4 = inBmp.getPixel(w, h);
+
+                if(w != 0 && h != 0) inPixel0 = inBmp.getPixel(w-1, h-1);
+                else inPixel0 = 0;
+                if(h != 0) inPixel1 = inBmp.getPixel(w, h-1);
+                else inPixel1 = 0;
+                if(w != width && h != 0) inPixel2 = inBmp.getPixel(w+1, h-1);
+                else inPixel2 = 0;
+                if(w != 0) inPixel3 = inBmp.getPixel(w-1, h);
+                else inPixel3 = 0;
+
+                if(w != width) inPixel5 = inBmp.getPixel(w+1, h);
+                else inPixel5 = 0;
+                if(w != 0 && h != height) inPixel6 = inBmp.getPixel(w-1, h+1);
+                else inPixel6 = 0;
+                if(w != 0 && h != height) inPixel7 = inBmp.getPixel(w, h+1);
+                else inPixel7 = 0;
+                if(w != width && h != height) inPixel8 = inBmp.getPixel(w+1, h+1);
+                else inPixel8 = 0;
+                outPixel = transformPixel(inPixel0, inPixel1, inPixel2, inPixel3,inPixel4,
+                        inPixel5, inPixel6, inPixel7, inPixel8);
+                newBmp.setPixel(w, h, outPixel);
+            }
+        }
+        return newBmp;
+    }
 }
